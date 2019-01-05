00e50ff47c8f156a5fb38f7393270e58f1d83202
unknown <faisalkhan.chat@gmail.com>
2019-01-05 06:28:33 -0800

    query param handling

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -18,7 +18,6 @@ export class EditServerComponent implements OnInit {
     ) { }
 
   ngOnInit() {
-
     this.route.queryParams.subscribe(
       (queryParam: Params) => {
         this.allowEdit = queryParam['allowEdit'] === '1' ? true : false;
@@ -26,10 +25,11 @@ export class EditServerComponent implements OnInit {
     );
     this.route.fragment.subscribe();
     // console.log(this.server.name);
+    console.log(this.allowEdit);
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serverService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
-    // const id = +this.route.snapshot.params['id'];
-    this.server = this.serverService.getServer(1);
   }
   onUpdateServer() {
 
